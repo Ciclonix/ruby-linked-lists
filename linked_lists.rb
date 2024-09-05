f7467cd0a08bddc513e9ea8eff4caa5b1abbcec3
Ciclonix <72094161+Ciclonix@users.noreply.github.com>
2024-09-05 15:03:45 +0200

    Create linked_lists.rb

---
+++ linked_lists.rb
@@ -0,0 +1,12 @@
+class LinkedList
+
+end
+
+class Node
+  attr_reader :value, :next_node
+
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
+end
