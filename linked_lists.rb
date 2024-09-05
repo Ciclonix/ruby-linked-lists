89b5fbedf43e93be054c7b9f0431669b52acdb4e
Ciclonix <72094161+Ciclonix@users.noreply.github.com>
2024-09-05 15:30:23 +0200

    Update classes Node and LinkedList

--- linked_lists.rb
+++ linked_lists.rb
@@ -1,12 +1,41 @@
-class LinkedList
-
-end
-
 class Node
   attr_reader :value, :next_node
 
   def initialize(value = nil, next_node = nil)
     @value = value
     @next_node = next_node
+  end
+
+  def getNextNode(node)
+    return @next_node
+  end
+
+  def changeNextNode(node)
+    @next_node = node
+  end
+end
+
+
+class LinkedList
+  def initialize
+    @head = nil
+  end
+
+  def getTail
+    node = @head
+    loop do
+      next_node = node.getNextNode(node)
+      break if next_node.nil?
+      node = next_node
+    end
+    return node
+  end
+
+  def append(value)
+    if @head.nil?
+      @head = Node.new(value)
+    else
+      getTail.changeNextNode(Node.new(value))
+    end
   end
 end
